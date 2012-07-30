67185c8ce7b177aa6991fdde543801ae38046d4c
Kirk Okada <kirkokada@gmail.com>
2012-07-30 13:31:07 +0900

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Flashcard::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
